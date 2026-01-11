683c97c4d5d9f7f18f071dbecab6fa44f878f385
henryas01 <ashenry325@gmail.com>
2026-01-12 04:01:47 +0700

    update user-agent

--- src/proxy.ts
+++ src/proxy.ts
@@ -2,6 +2,11 @@ import { NextResponse, NextRequest } from 'next/server'
 
 
 export function proxy(request: NextRequest) {
+   const ua = request.headers.get("user-agent") || "";
+
+  if (ua.includes("Googlebot")) {
+    return NextResponse.next();
+  }
   if (request.nextUrl.pathname.startsWith('/about')  || request.nextUrl.pathname === '/' ||request.nextUrl.pathname.startsWith('/admin') || request.nextUrl.pathname.startsWith('/customer')  ) {
 
     return NextResponse.redirect(new URL('/home', request.url));
