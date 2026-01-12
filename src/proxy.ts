c6d5746270e5e188ac2864b791c89da274206c89
henryas01 <ashenry325@gmail.com>
2026-01-13 01:26:46 +0700

    update

--- src/proxy.ts
+++ src/proxy.ts
@@ -2,19 +2,21 @@ import { NextResponse, NextRequest } from 'next/server'
 
 
 export function proxy(request: NextRequest) {
-  //  const ua = request.headers.get("user-agent") || "";
+  const { pathname } = request.nextUrl;
 
-  // if (ua.includes("Googlebot")) {
-  //   return NextResponse.next();
-  // }
-  if (request.nextUrl.pathname.startsWith('/about')  || request.nextUrl.pathname === '/' ||request.nextUrl.pathname.startsWith('/admin') || request.nextUrl.pathname.startsWith('/customer')  ) {
+  if (pathname.startsWith("/about")) {
+    return NextResponse.redirect(new URL("/home", request.url));
+  }
 
-    return NextResponse.redirect(new URL('/home', request.url));
+  // Disable admin & customer redirection to home page currently
+  if (pathname.startsWith("/admin") || pathname.startsWith("/customer")) {
+    return NextResponse.redirect(new URL("/home", request.url));
   }
 
   return NextResponse.next();
+
 }
 
-// export const config = {
-//   matcher: '/about/:path*',
-// }
+export const config = {
+   matcher: ["/about/:path*", "/admin/:path*", "/customer/:path*"],
+};
